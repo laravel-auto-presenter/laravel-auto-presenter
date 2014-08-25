dc843d315033d5b4e3d56ec562c8c2b2d85b62a0
kirkbushell <torm3nt@gmail.com>
2014-08-25 14:21:08 +0100

    Model test added for simple atom relations

---
+++ tests/Decorators/AtomDecoratorTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace McCool\Tests\Decorators;
+
+use McCool\LaravelAutoPresenter\Decorators\AtomDecorator;
+
+use Mockery as m;
+
+class AtomDecoratorTest extends \PHPUnit_Framework_TestCase
+{
+	private $atomDecorator;
+
+	public function setUp()
+	{
+		$this->atomDecorator = new AtomDecorator;
+	}
+
+	public function testCanDecorateModel()
+	{
+		$model = m::mock('Illuminate\Database\Eloquent\Model');
+
+		$this->assertTrue($this->atomDecorator->canDecorate($model));
+	}
+
+	public function testCanDecoratePresenterInterface()
+	{
+		$subject = m::mock('McCool\LaravelAutoPresenter\PresenterInterface');
+
+		$this->assertTrue($this->atomDecorator->canDecorate($subject));
+	}
+
+	public function testCannotDecorateGarbage()
+	{
+		$this->assertFalse($this->atomDecorator->canDecorate([]));
+		$this->assertFalse($this->atomDecorator->canDecorate(null));
+	}
+
+	public function testShouldHandleRelationsWhenSubjectIsAModel()
+	{
+		$model = m::mock('Illuminate\Database\Eloquent\Model');
+		$collection = ['blah'];
+
+		$model->shouldReceive('getRelations')->andReturn($collection);
+		$model->shouldReceive('setRelation')->with(0, $collection[0]);
+
+		$this->atomDecorator->decorate($model);
+	}
+}
