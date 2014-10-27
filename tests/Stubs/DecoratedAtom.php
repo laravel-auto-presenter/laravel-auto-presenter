e59e22b674435bb6d729b76f9a3f482247971b59
Graham Campbell <graham@mineuk.com>
2014-10-27 12:25:07 +0000

    Ditched a docblock

--- tests/Stubs/DecoratedAtom.php
+++ tests/Stubs/DecoratedAtom.php
@@ -6,11 +6,6 @@ class DecoratedAtom implements HasPresenter
 {
     public $myProperty = "bazinga";
 
-    /**
-     * Get the presenter class.
-     *
-     * @return string The class path to the presenter.
-     */
     public function getPresenterClass()
     {
         return DecoratedAtomPresenter::class;
