911066cf2848a97bdda7254985896b2099598dda
Graham Campbell <graham@mineuk.com>
2015-01-01 01:24:17 +0000

    Added file headers

--- src/HasPresenter.php
+++ src/HasPresenter.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * This file is part of Laravel Auto Presenter.
+ *
+ * (c) Shawn McCool <shawn@heybigname.com>
+ * (c) Graham Campbell <graham@mineuk.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace McCool\LaravelAutoPresenter;
 
 interface HasPresenter
