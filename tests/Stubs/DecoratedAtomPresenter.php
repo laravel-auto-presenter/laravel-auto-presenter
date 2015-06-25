0f2d7344ab2ce35b61e31f3be459a5f185ed5dcb
Graham Campbell <graham@alt-three.com>
2015-06-25 14:17:05 +0100

    Updated email addresses

--- tests/Stubs/DecoratedAtomPresenter.php
+++ tests/Stubs/DecoratedAtomPresenter.php
@@ -4,7 +4,7 @@
  * This file is part of Laravel Auto Presenter.
  *
  * (c) Shawn McCool <shawn@heybigname.com>
- * (c) Graham Campbell <graham@cachethq.io>
+ * (c) Graham Campbell <graham@alt-three.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
