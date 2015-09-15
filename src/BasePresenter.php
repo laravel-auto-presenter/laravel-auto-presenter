b0ea4b070a31cca379c845fdd76b92772341afff
Roy de Vos Burchart <roy.devosburchart@comayers.com>
2015-09-14 21:45:56 -0500

    Implement UrlRoutable for getRouteKey

--- src/BasePresenter.php
+++ src/BasePresenter.php
@@ -13,8 +13,9 @@
 namespace McCool\LaravelAutoPresenter;
 
 use McCool\LaravelAutoPresenter\Exceptions\MethodNotFoundException;
+use Illuminate\Contracts\Routing\UrlRoutable;
 
-abstract class BasePresenter
+abstract class BasePresenter implements UrlRoutable
 {
     /**
      * The resource that is the object that was decorated.
@@ -43,6 +44,26 @@ abstract class BasePresenter
     public function getWrappedObject()
     {
         return $this->wrappedObject;
+    }
+
+    /**
+     * Get the value of the model's route key.
+     *
+     * @return mixed
+     */
+    public function getRouteKey()
+    {
+        return $this->wrappedObject->getRouteKey();
+    }
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return $this->wrappedObject->getRouteKeyName();
     }
 
     /**
